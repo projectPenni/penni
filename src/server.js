4d3887e4ebbb28b0372141d29c1dc9833af145c3
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-11-23 01:47:04 +0400

    Small changes

--- src/server.js
+++ src/server.js
@@ -42,10 +42,10 @@ function getConversationReply(message, _id) {
         }
         const data = {entities: response.entities, intents: response.intents};
         if (response.output.text.length === 0) {
-          resolve("I don't know what to say...");
+          resolve("I don't know what to say. Could you please rephrase that?");
         }
         else if (_.isEqual(lastResponse, data)) {
-          resolve("But... you already said that? :(");
+          resolve("But... you already said that?");
         }
         else {
           const reply = response.output.text.join('SPLITPOINT');
